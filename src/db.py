e9730c03f8330de698c4eed0e3ad68a31c2a8081
Jason Jiang <jasonjiang@Jasons-MacBook-Pro-2.local>
2024-10-12 15:51:42 -0400

    env var

--- src/db.py
+++ src/db.py
@@ -2,9 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 
-cred_path = os.path.join(os.path.dirname(__file__), "serviceAccountKey.json")
-
-cred = credentials.Certificate(cred_path)
+cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
