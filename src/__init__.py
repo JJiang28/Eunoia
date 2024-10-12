e026cb0524e657ba17047041d54920283289a38b
Jason Jiang <jasonjiang@Jasons-MacBook-Pro-2.local>
2024-10-12 16:24:21 -0400

    boiler

--- src/__init__.py
+++ src/__init__.py
@@ -10,7 +10,6 @@ def create_app(test_config=None):
 
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
