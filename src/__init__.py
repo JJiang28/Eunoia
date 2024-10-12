206e087d954a93ef856021c29fcf846528b90ed6
Jason Jiang <jasonjiang@Jasons-MacBook-Pro-2.local>
2024-10-12 16:22:37 -0400

    env

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask
 from .db import db
+from dotenv import load_dotenv
 
 def create_app(test_config=None):
+    load_dotenv()
+
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
@@ -30,9 +33,9 @@ def create_app(test_config=None):
         try:
             doc_ref = db.collection('schedules').document('example_doc')
             doc_ref.set({
-                'title': 'Finish Firestore Setup',
-                'due_date': '2024-10-11',
-                'status': 'In Progress'
+                'title': 'Finish Firestore Setup3',
+                'due_date': '2024-10-13',
+                'status': 'In Progress3'
             })
             return 'Document added to Firestore!', 200
         except Exception as e:
