9288bbe235e1cadc9d594532b62de32f896540f4
Jason Jiang <jiangjason28@gmail.com>
2024-10-13 12:41:53 -0400

    testing commit history

--- src/app/routes.py
+++ src/app/routes.py
@@ -6,17 +6,23 @@ app_routes = Blueprint('app_routes', __name__)
 def hello():
     return 'Welcome to Eunoia'
 
-@app_routes.route('/add-example', methods=['POST'])
-def add_example():
-    """Simple route to add a document to Firestore for testing."""
-    try:
-        db = current_app.config['FIRESTORE_DB']
-        doc_ref = db.collection('schedules').document('example_doc')
-        doc_ref.set({
-            'title': 'Finish Firestore Setup',
-            'due_date': '2024-10-11',
-            'status': 'In Progress'
-        })
-        return 'Document added to Firestore!', 200
-    except Exception as e:
-        return f"An Error Occurred: {e}", 500
+# @app_routes.route('/add-habit')
+# def add_habit():
+#     try:
+#         db = current_app.config['FIRESTORE_DB']
+#         doc_ref = db.collection('')
+
+# @app_routes.route('/add-example', methods=['POST'])
+# def add_example():
+#     """Simple route to add a document to Firestore for testing."""
+#     try:
+#         db = current_app.config['FIRESTORE_DB']
+#         doc_ref = db.collection('schedules').document('example_doc')
+#         doc_ref.set({
+#             'title': 'Finish Firestore Setup',
+#             'due_date': '2024-10-11',
+#             'status': 'In Progress'
+#         })
+#         return 'Document added to Firestore!', 200
+#     except Exception as e:
+#         return f"An Error Occurred: {e}", 500
